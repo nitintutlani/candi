0a716b9f5522d4f6becfd4f95b72db4a8cfccfc4
Nitin Tutlani <nitintutlani@yahoo.com>
2013-12-20 02:43:29 +0530

    new Injection class

---
+++ lib/Injection.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Nitin Tutlani <nitintutlani@yahoo.com>
+ */
+
+/**
+ * Injectable object class
+ *
+ * Every container has _injections array that collects these Injection objects
+ * Removed `depends`, provider functions now inject invokable functions into value as returned from Util.annotateFn
+ */
+class Injection {
+    public type: string;
+    public name: string;
+    public value: any;
+    public _cache: any;
+
+    constructor(type: string, name: string, value: any) {
+        this.type = type;
+        this.name = name;
+        this.value = value;
+    }
+
+}
+
+export = Injection;
